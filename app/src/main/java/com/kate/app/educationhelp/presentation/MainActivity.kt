1fafcc06c7efc6d37bf6a66832f066a880176b16
Katesmir04 <Katecmirnova0407@mail.ru>
2021-02-24 14:20:48 +0300

    Retrofit API client, simple request to backend for all topics

--- app/src/main/java/com/kate/app/educationhelp/presentation/MainActivity.kt
+++ app/src/main/java/com/kate/app/educationhelp/presentation/MainActivity.kt
@@ -2,11 +2,19 @@ package com.kate.app.educationhelp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.kate.app.educationhelp.R
+import com.kate.app.educationhelp.data.repositories.MyBackendRepository
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch {
+           Log.d("KEK", MyBackendRepository.getAllTopics().toString())
+        }
     }
 }
