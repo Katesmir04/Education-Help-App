2b346b2667944db6443051884f19e01e8306e88a
Katesmir04 <Katecmirnova0407@mail.ru>
2021-02-24 15:06:14 +0300

    AllTopicsViewModel.kt

--- app/src/main/java/com/kate/app/educationhelp/presentation/AllTopicsViewModel.kt
+++ app/src/main/java/com/kate/app/educationhelp/presentation/AllTopicsViewModel.kt
@@ -1,4 +1,28 @@
 package com.kate.app.educationhelp.presentation
 
-class AllTopicsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kate.app.educationhelp.data.repositories.MyBackendRepository
+import com.kate.app.educationhelp.domain.usecases.GetAllTopicsUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AllTopicsViewModel : ViewModel() {
+
+    private val _topicsState = MutableLiveData<TopicsListState>(TopicsListState.Loading)
+    val topicsState: LiveData<TopicsListState>
+        get() = _topicsState
+
+    init {
+        refreshData()
+    }
+
+    fun refreshData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _topicsState.value =
+                TopicsListState.Loaded(GetAllTopicsUseCase(MyBackendRepository).execute())
+        }
+    }
 }
