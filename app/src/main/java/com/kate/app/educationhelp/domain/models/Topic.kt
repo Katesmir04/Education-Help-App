bc0df144cd54abc20da2d1062cdf2b34ccb2f17d
Katesmir04 <Katecmirnova0407@mail.ru>
2021-04-25 16:02:47 +0300

    quize instance

--- app/src/main/java/com/kate/app/educationhelp/domain/models/Topic.kt
+++ app/src/main/java/com/kate/app/educationhelp/domain/models/Topic.kt
@@ -10,10 +10,10 @@ data class Topic(
     var body: String? = null,
     var images: List<String>? = null,
     var videos: List<String>? = null,
-    var testsId: List<String>? = null,
+    var quizeIds: List<String>? = null,
     var grade: Int? = null,
     var subject: String? = null
 
-):Parcelable {
+) : Parcelable {
 
 }
